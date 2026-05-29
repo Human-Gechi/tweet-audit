1a6eee1674682f0326c00a77c6ea324d0b02a656
Human-Gechi <kimberlydaniel363@gmail.com>
2026-05-29 11:54:22 +0100

    test_archiveparser

--- tests/test_archive_parser.py
+++ tests/test_archive_parser.py
@@ -2,7 +2,35 @@ from unittest.mock import MagicMock, patch
 from src.archive_parser import unzip
 import pytest
 
-@patch("src.archive_parser.zipfile.is_Zipfile")
-@patch("src.archive_parser.zipfile.is_Zipfile")
-def test_unzip():
-    pass
+
+@patch("src.archive_parser.logger")
+@patch("src.archive_parser.zipfile.ZipFile")
+@patch("src.archive_parser.Path")
+def test_unzip_directory_exists(mock_Path, mock_ZipFile, mock_logger):
+
+    mock_path_instance = MagicMock()
+    mock_path_instance.exists.return_value = True
+    mock_Path.return_value = mock_path_instance
+
+    unzip(zip_path="test.zip", extract_to="test_dir")
+
+    mock_path_instance.exists.assert_called_once()
+
+    mock_ZipFile.assert_not_called()
+
+    mock_logger.info.assert_called_with("File exists skipping Unzipping")
+
+@patch("src.archive_parser.zipfile.is_zipfile")
+@patch("src.archive_parser.Path")
+def test_unzip_invalid_zipfile(mock_Path, mock_is_zipfile):
+
+    mock_path_instance = MagicMock()
+    mock_path_instance.exists.return_value = False
+    mock_Path.return_value = mock_path_instance
+    zip_path = "not_a_zip.txt"
+    mock_is_zipfile.return_value = False
+
+    with pytest.raises(ValueError) as err:
+        unzip(zip_path=zip_path, extract_to="tweet_data")
+
+    assert f"{zip_path} is not a valid zip file" in str(err.value)
